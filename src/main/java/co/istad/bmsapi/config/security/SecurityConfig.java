350f7d51f096b328ec4bc85596c791bf65c80ae2
it.chhaya <it.chhaya@gmail.com>
2022-11-21 09:06:57 +0700

    remove security warning

--- src/main/java/co/istad/bmsapi/config/security/SecurityConfig.java
+++ src/main/java/co/istad/bmsapi/config/security/SecurityConfig.java
@@ -1,7 +1,5 @@
 package co.istad.bmsapi.config.security;
 
-import java.util.Arrays;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -14,9 +12,6 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -83,15 +78,5 @@ public class SecurityConfig {
 
         return http.build();
     }
-
-    // @Bean
-    // CorsConfigurationSource corsConfigurationSource() {
-    //     CorsConfiguration configuration = new CorsConfiguration();
-    //     configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000/"));
-    //     configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
-    //     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    //     source.registerCorsConfiguration("/**", configuration);
-    //     return source;
-    // }
 
 }
